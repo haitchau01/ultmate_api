b57364d1b5a62a812c98d83f23751164a926cb63
loki <haichau.forgit@gmail.com>
2025-03-02 21:39:00 +0700

    test

---
+++ CharsetChecker/CharsetChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using MimeKit;
+using System.Text;
+using UtfUnknown;
+
+var path = @"E:\file_test_eml\testfile.text";
+
+var byteData = File.ReadAllBytes(path);
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var encode = Encoding.GetEncoding("iso-2022-jp").GetString(byteData);
+// Thử phát hiện charset tự động
+DetectionResult result = CharsetDetector.DetectFromBytes(byteData);
+
+Console.WriteLine(result.Detected);
+
+
+
